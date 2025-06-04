7ffb175143037d4a04707d25436d8d73a6ae2f23
Alvaro <alvarojimgil12@gmail.com>
2025-06-04 16:04:49 +0200

    version final 2

--- gamerbox-frontend/vite.config.js
+++ gamerbox-frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
-    allowedHosts: ["freelance-frontend"],
+    allowedHosts: ["frontend"],
   },
 });
